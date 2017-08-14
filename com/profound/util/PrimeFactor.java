446bd6a758773c73a481fe570f2c6b1787e27706
panprem <30771144+panprem@users.noreply.github.com>
2017-08-14 20:34:03 +0530

    PrimeFactor

---
+++ com/profound/util/PrimeFactor.java
@@ -0,0 +1,38 @@
+/****************************************
+ *Program:prime factor*
+ *Author: Prem*
+ *Version:1.0*
+*****************************************/
+//package com.profound.util;
+
+public class PrimeFactor 
+{
+    static   int number;
+     //counter 
+    static int ctr=0;
+  /*controlling loop*/
+    static int i,j;
+  public static void main(String [] args)
+  {
+     number=Integer.parseInt(args[0]);
+      for(i=1;i*i<=number;i++)
+      {
+        if(number%i==0)
+        {
+           for(j=1;j<i-1;j++)
+           {  
+               if(i%j==0)
+                {
+                 continue;
+                }
+               else
+                {
+                   System.out.println(i);
+                }
+             
+           }
+
+        }
+     }
+  }
+}
