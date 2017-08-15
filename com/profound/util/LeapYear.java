bc326fd37e156946d0fc427bb7bc483b16df58f6
panprem <30771144+panprem@users.noreply.github.com>
2017-08-15 18:15:44 +0530

    LeapYear

---
+++ com/profound/util/LeapYear.java
@@ -0,0 +1,38 @@
+/******************************************************************************
+ *  Compilation:  javac -d bin com/profound/util/LeapYear.java
+ *  Execution:    java -cp bin com.profound.util.LeapYear n
+ *  @author  Prem
+ *  @version 1.0
+ *  
+ *  Prints true if n corresponds to a leap year, and false otherwise.
+ *  Assumes n >= 1582, corresponding to a year in the Gregorian calendar.
+ *
+ *  % java LeapYear 2004
+ *  2004 is a leap year
+ *
+ *  % java LeapYear 1900
+ *  1900 is not a leap year
+ ******************************************************************************/
+//package com.profound.util;
+import java.util.Scanner;
+
+public class LeapYear
+{
+	// variables
+		static int year;
+	public static void main(String [] args)
+	{
+		//Taking the year input as command line argument
+		year=Integer.parseInt(args[0]);
+
+		if((year%400==0) || ((year%4==0) && (year%100!=0)))
+		{
+			System.out.println(year+" is a leap year");
+		}
+		else
+		{
+			System.out.println(year+" is not a leap year");
+		}
+		
+	}
+}
